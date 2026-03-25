ea81d14581793e6f53602c7616a5927374a1728b
mozes-lol <johnmarkgarcia164@gmail.com>
2026-03-25 21:39:16 +0800

    Implement boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Mozes is a nigger</h1>
+      <p>Edit src/App.jsx and save to test HMR.</p>
+    </main>
+  )
+}
+
+export default App
