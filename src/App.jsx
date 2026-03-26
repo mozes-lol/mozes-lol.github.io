f987ab50b8f449f54dd88caabb120c67feaa81ea
mozes-lol <johnmarkgarcia164@gmail.com>
2026-03-26 10:51:37 +0800

    Download and import Bootstrap stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <main className="app">
-      <h1>Mozes is a nigger</h1>
-      <p>Edit src/App.jsx and save to test HMR.</p>
+      <h1 className="display-4, text-center">Mozes is a nigger</h1>
+      <p className="lead">Edit src/App.jsx and save to test HMR.</p>
     </main>
   )
 }
