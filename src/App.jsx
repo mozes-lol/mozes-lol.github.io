a5dfc62b45894bc4d1584d98fa50ccb4a1b9a84c
mozes-lol <johnmarkgarcia164@gmail.com>
2026-03-26 11:40:35 +0800

    Create timeline placeholder
    This could be useful to record previous history of projects, education, and other notable things in my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import Button from '@mui/material/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import MyEducation from './components/MyEducation.jsx';
+
 
 function App() {
   return (
     <main className="app">
-      <h1 className="display-4, text-center">Mozes is a nigger</h1>
-      <p className="lead, text-center">Edit src/App.jsx and save to test HMR.</p>
-      <div className="d-flex justify-content-center">
-        <Button variant="contained">Hello world</Button>
-      </div>
+      <MyEducation />
     </main>
   )
 }
---
+++ src/components/MyEducation.jsx
@@ -0,0 +1,59 @@
+import Button from '@mui/material/Button';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+function MyEducation() {
+  return (
+    <Timeline position="right">
+        <h1 className='text-center'>My Education</h1>
+        <TimelineItem>
+            <TimelineOppositeContent color="text.primary">
+                <h2>2023-Present</h2>
+            </TimelineOppositeContent>
+        <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+            <h1>Mapua University</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit architecto minima laboriosam illum unde eligendi, esse sequi eum exercitationem sapiente deserunt quod numquam? Laboriosam obcaecati ipsum voluptas similique! Pariatur, ducimus!</p>
+            <Button variant="contained" color="primary">Learn More</Button>
+        </TimelineContent>
+        </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.primary">
+            <h2>2021-2023</h2>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+            <h1>iACADEMY</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit architecto minima laboriosam illum unde eligendi, esse sequi eum exercitationem sapiente deserunt quod numquam? Laboriosam obcaecati ipsum voluptas similique! Pariatur, ducimus!</p>
+            <Button variant="contained" color="primary">Learn More</Button>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.primary">
+          <h2>2021-2013</h2>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>
+          <h1>Notre Dame of Greater Manila</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit architecto minima laboriosam illum unde eligendi, esse sequi eum exercitationem sapiente deserunt quod numquam? Laboriosam obcaecati ipsum voluptas similique! Pariatur, ducimus!</p>
+          <Button variant="contained" color="primary">Learn More</Button>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  )
+}
+
+export default MyEducation
