6d25ea7c99c5ba55ac8f32e602091fd7e0f51ea9
mozes-lol <johnmarkgarcia164@gmail.com>
2026-03-26 11:09:36 +0800

    Testing JoyUI button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <main className="app">
       <h1 className="display-4, text-center">Mozes is a nigger</h1>
       <p className="lead, text-center">Edit src/App.jsx and save to test HMR.</p>
+      <div className="d-flex justify-content-center">
+        <Button variant="contained">Hello world</Button>
+      </div>
     </main>
   )
 }
